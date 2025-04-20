dea39c3592115a41b4e3e9c9b6e4659f7ed17e2c
ianSama0217 <ian20000217@gmail.com>
2025-04-20 22:12:54 +0800

    JWT config

---
+++ src/main/java/com/example/chat/jwt/JwtUtil.java
@@ -0,0 +1,52 @@
+package com.example.chat.jwt;
+
+import java.security.Key;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+
+@Component
+public class JwtUtil {
+	private final String KEY = "JWTKEY20250420CONFIGAHTHORIAN_SECRETKEY!";
+	private final Key SECRET_KEY = Keys.hmacShaKeyFor(KEY.getBytes());
+
+	public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1天
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration()
+                .before(new Date());
+    }
+}
--- src/main/java/com/example/chat/security/SecurityConfig.java
+++ src/main/java/com/example/chat/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.chat.security;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,10 +12,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.example.chat.jwt.JwtAuthFilter;
 
 @Configuration
 @EnableWebSecurity(debug = true)
 public class SecurityConfig {
+	
+	@Autowired
+	private JwtAuthFilter jwtAuthFilter;
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -22,21 +34,30 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 		
-		return http
-				.sessionManagement(session -> session
-						.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
-				
-				.csrf(csrf -> csrf.disable())
-				.httpBasic(Customizer.withDefaults())
-				// 關閉預設登入表單
-				.formLogin(form -> form.disable())
-				
-				.authorizeHttpRequests(request -> request
-						.requestMatchers("/register", "/test").permitAll()
-						.requestMatchers("/login", "/hello").authenticated()
-						.anyRequest().denyAll()
-				)
-				
-				.build();
+	    return http
+	            .csrf(csrf -> csrf.disable())
+	            .httpBasic(httpBasic -> httpBasic.disable()) // 可選，JWT 不需要 HTTP Basic
+	            .formLogin(form -> form.disable()) // 關閉表單登入
+	            .authorizeHttpRequests(request -> request
+	                .requestMatchers("/register", "/login", "/test").permitAll()
+	                .requestMatchers("/hello", "user/**").authenticated()
+	                .anyRequest().denyAll()
+	            )
+	            .cors(cors -> cors.configurationSource(createCorsConfig()))
+	            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+	            .build();
+	}
+	
+	private CorsConfigurationSource createCorsConfig() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(List.of("*")); // 允許那些來源請求
+		config.setAllowedHeaders(List.of("*"));
+		config.setAllowedMethods(List.of("*"));
+		config.setMaxAge(3600L); // 設定preflight請求結果可以被瀏覽器cache幾秒
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		
+		return source;
 	}
 }
